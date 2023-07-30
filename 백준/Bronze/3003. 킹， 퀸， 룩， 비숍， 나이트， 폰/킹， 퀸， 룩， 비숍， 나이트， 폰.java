500ead72d425348831160a926605831f62c291e1
jyc0011 <122250128+jyc0011@users.noreply.github.com>
2023-07-30 18:20:04 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 236 ms, Memory: 18308 KB -BaekjoonHub

---
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] correctSet = {1, 1, 2, 2, 2, 8};
+
+        for (int i = 0; i < 6; i++) {
+            int input = sc.nextInt();
+            System.out.print((correctSet[i] - input) + " ");
+        }
+    }
+}
