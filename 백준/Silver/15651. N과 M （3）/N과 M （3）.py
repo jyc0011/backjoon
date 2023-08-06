357e50ecd0a04fcc74322c73a5c1b932bdcd815d
jyc0011 <122250128+jyc0011@users.noreply.github.com>
2023-08-06 14:52:04 +0900

    [Silver III] Title: N과 M (3), Time: 1420 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.py
@@ -0,0 +1,12 @@
+n,m = list(map(int,input().split()))
+s = []
+def go():
+    if len(s)==m:
+        print(' '.join(map(str,s)))
+        return
+    
+    for i in range(1,n+1):
+        s.append(i)
+        go()
+        s.pop()
+go()
