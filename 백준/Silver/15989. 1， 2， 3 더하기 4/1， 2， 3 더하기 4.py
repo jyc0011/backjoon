2969dd289ef562803d9fbfd7e833cd52ed22deff
jyc0011 <122250128+jyc0011@users.noreply.github.com>
2023-08-13 18:57:26 +0900

    [Silver I] Title: 1, 2, 3 더하기 4, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/15989. 1， 2， 3 더하기 4/1， 2， 3 더하기 4.py
@@ -0,0 +1,14 @@
+import sys
+
+
+dp = [1] * 10001
+for i in range(2, 10001):
+    dp[i] += dp[i-2]
+
+for i in range(3, 10001):
+    dp[i] += dp[i-3]
+
+T = int(sys.stdin.readline().strip())
+for _ in range(T):
+    n = int(sys.stdin.readline().strip())
+    print(dp[n])
