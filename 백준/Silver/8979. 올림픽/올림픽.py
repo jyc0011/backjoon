edfea41a4d68f0dfd8abff6ecbbd45a0f26c5a41
jyc0011 <122250128+jyc0011@users.noreply.github.com>
2023-08-15 19:11:30 +0900

    [Silver V] Title: 올림픽, Time: 36 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.py
+++ 백준/Silver/8979. 올림픽/올림픽.py
@@ -1,15 +1,18 @@
 import sys
 
-n, k = map(int, sys.stdin.readline().split())
-countries = []
+M, N = map(int, sys.stdin.readline().split())
+arr = []
 
-for _ in range(n):
-    country, gold, silver, bronze = map(int, sys.stdin.readline().split())
-    countries.append((gold, silver, bronze, country))
+for _ in range(M):
+    data = list(map(int, sys.stdin.readline().split()))
+    arr.append(data)
 
-countries.sort(reverse=True)
+s_arr = sorted(arr, key=lambda x: (-x[1], -x[2], -x[3]))
 
-for i in range(n):
-    if countries[i][3] == k:
-        print(i + 1)
+for c in arr:
+    if c[0] == N:
+        target = c
         break
+
+rank = s_arr.index(target) + 1
+print(rank)
