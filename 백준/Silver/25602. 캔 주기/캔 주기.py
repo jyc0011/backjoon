071c87e50d87557c0555e851ddf1960b2853dde7
jyc0011 <122250128+jyc0011@users.noreply.github.com>
2023-08-15 17:57:09 +0900

    [Silver I] Title: 캔 주기, Time: 76 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/25602. 캔 주기/캔 주기.py
+++ 백준/Silver/25602. 캔 주기/캔 주기.py
@@ -1,32 +1,23 @@
 import sys
 
 N, K = map(int, sys.stdin.readline().strip().split())
-A = [0] + list(map(int, sys.stdin.readline().strip().split()))
-R = [[0] + list(map(int, sys.stdin.readline().strip().split())) for _ in range(K)]
-M = [[0] + list(map(int, sys.stdin.readline().strip().split())) for _ in range(K)]
+A = list(map(int, sys.stdin.readline().strip().split()))
+R = [list(map(int, sys.stdin.readline().strip().split())) for _ in range(K)]
+M = [list(map(int, sys.stdin.readline().strip().split())) for _ in range(K)]
 ans = 0
 
-def dfs(d, r, m):
+def dfs(day, total_satisfaction):
     global ans
-    if d > K:
-        ans = max(ans, r + m)
+    if day == K:
+        ans = max(ans, total_satisfaction)
         return
 
-    for i in range(1, N + 1):
-        nr = r
-        nm = m
-        if A[i] > 0:
-            A[i] -= 1
-            nr += R[d-1][i]
-            for j in range(1, N + 1):
-                if A[j] > 0:
-                    A[j] -= 1
-                    nm += M[d-1][j]
-                    dfs(d + 1, nr, nm)
-                    nm -= M[d-1][j]
-                    A[j] += 1
-            nr -= R[d-1][i]
-            A[i] += 1
+    for i in range(N):
+        for j in range(N):
+            if (i == j and A[i] >= 2) or (i != j and A[i] >= 1 and A[j] >= 1):
+                A[i] -= 1; A[j] -= 1
+                dfs(day + 1, total_satisfaction + R[day][i] + M[day][j])
+                A[i] += 1; A[j] += 1
 
-dfs(1, 0, 0)
+dfs(0, 0)
 print(ans)
