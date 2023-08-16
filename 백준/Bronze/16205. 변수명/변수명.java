f26e4c4b24a9f14c1f29d0f8a5fabec4fe44b0b0
jyc0011 <122250128+jyc0011@users.noreply.github.com>
2023-08-16 09:50:20 +0900

    [Bronze I] Title: 변수명, Time: 124 ms, Memory: 14244 KB -BaekjoonHub

--- 백준/Bronze/16205. 변수명/변수명.java
+++ 백준/Bronze/16205. 변수명/변수명.java
@@ -1,12 +1,12 @@
 import java.io.*;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] input = br.readLine().split(" ");
-        int choice = Integer.parseInt(input[0]);
-        String original = input[1];
-        
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int choice = Integer.parseInt(st.nextToken());
+        String original = st.nextToken();
         StringBuilder sb = new StringBuilder(original);
 
         switch (choice){
