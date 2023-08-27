187465da97eabb5ca530faa9ad7890222fdad21c
jyc0011 <122250128+jyc0011@users.noreply.github.com>
2023-08-27 16:17:34 +0900

    [Gold V] Title: 배열 돌리기 3, Time: 1052 ms, Memory: 106124 KB -BaekjoonHub

---
+++ 백준/Gold/16935. 배열 돌리기 3/배열 돌리기 3.java
@@ -0,0 +1,113 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        int N = scanner.nextInt();
+        int M = scanner.nextInt();
+        int R = scanner.nextInt();
+        
+        int[][] arr = new int[N][M];
+        
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                arr[i][j] = scanner.nextInt();
+            }
+        }
+        
+        for (int i = 0; i < R; i++) {
+            int op = scanner.nextInt();
+            if (op == 1) {
+                arr = flip(rotate90(rotate90(arr)));
+            } else if (op == 2) {
+                arr = flip(arr);
+            } else if (op == 3) {
+                int temp = N;
+                N = M;
+                M = temp;
+                arr = rotate90(arr);
+            } else if (op == 4) {
+                int temp = N;
+                N = M;
+                M = temp;
+                arr = rotate90(rotate90(rotate90(arr)));
+            } else if (op == 5) {
+                int[][][] divided = divide(arr);
+                arr = combine(divided[2], divided[0], divided[3], divided[1]);
+            } else if (op == 6) {
+                int[][][] divided = divide(arr);
+                arr = combine(divided[1], divided[3], divided[0], divided[2]);
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static int[][] rotate90(int[][] matrix) {
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        int[][] result = new int[cols][rows];
+        
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                result[j][rows - 1 - i] = matrix[i][j];
+            }
+        }
+        
+        return result;
+    }
+
+    public static int[][] flip(int[][] matrix) {
+        int rows = matrix.length;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0, k = matrix[i].length - 1; j < k; j++, k--) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][k];
+                matrix[i][k] = temp;
+            }
+        }
+        return matrix;
+    }
+
+    public static int[][][] divide(int[][] array) {
+        int n = array.length;
+        int m = array[0].length;
+        
+        int[][] q1 = new int[n/2][m/2];
+        int[][] q2 = new int[n/2][m/2];
+        int[][] q3 = new int[n/2][m/2];
+        int[][] q4 = new int[n/2][m/2];
+        
+        for (int i = 0; i < n/2; i++) {
+            for (int j = 0; j < m/2; j++) {
+                q1[i][j] = array[i][j];
+                q2[i][j] = array[i][j + m/2];
+                q3[i][j] = array[i + n/2][j];
+                q4[i][j] = array[i + n/2][j + m/2];
+            }
+        }
+        return new int[][][]{q1, q2, q3, q4};
+    }
+
+    public static int[][] combine(int[][] q1, int[][] q2, int[][] q3, int[][] q4) {
+        int n = q1.length + q3.length;
+        int m = q1[0].length + q2[0].length;
+        
+        int[][] result = new int[n][m];
+        for (int i = 0; i < n/2; i++) {
+            for (int j = 0; j < m/2; j++) {
+                result[i][j] = q1[i][j];
+                result[i][j + m/2] = q2[i][j];
+                result[i + n/2][j] = q3[i][j];
+                result[i + n/2][j + m/2] = q4[i][j];
+            }
+        }
+        return result;
+    }
+}
