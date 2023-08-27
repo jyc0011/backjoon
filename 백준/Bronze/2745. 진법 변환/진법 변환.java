55aed241ab513212eae5721ca7855947a78eec2a
jyc0011 <122250128+jyc0011@users.noreply.github.com>
2023-08-27 09:27:21 +0900

    [Bronze II] Title: 진법 변환, Time: 208 ms, Memory: 17744 KB -BaekjoonHub

---
+++ 백준/Bronze/2745. 진법 변환/진법 변환.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        String N = scanner.next();
+        int B = scanner.nextInt();
+        
+        int result = Integer.parseInt(N, B);
+        System.out.println(result);
+    }
+}
