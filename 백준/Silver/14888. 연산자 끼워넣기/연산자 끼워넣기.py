52a31ab6cc9e19fe5685e0ea259bd4a525da7cbe
jyc0011 <122250128+jyc0011@users.noreply.github.com>
2023-09-09 13:29:40 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 60 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
@@ -1,39 +1,28 @@
 import sys
-from itertools import permutations
 
 n = int(sys.stdin.readline())
-a = list(map(int, sys.stdin.readline().rstrip().split()))
-add, sub, mul,div = map(int, sys.stdin.readline().rstrip().split())
+a = list(map(int, sys.stdin.readline().split()))
+ad, s, m, d = map(int, sys.stdin.readline().split())
 
-op = []
+mx = -float('inf')
+mn = float('inf')
 
-op += ['+'] * add
-op += ['-'] * sub
-op += ['*'] * mul
-op += ['/'] * div
+def bt(i, r, ad, s, m, d):
+    global mx, mn
+    if i == n:
+        mx = max(mx, r)
+        mn = min(mn, r)
+        return
 
-max_val = -float('inf')
-min_val = float('inf')
+    if ad:
+        bt(i+1, r+a[i], ad-1, s, m, d)
+    if s:
+        bt(i+1, r-a[i], ad, s-1, m, d)
+    if m:
+        bt(i+1, r*a[i], ad, s, m-1, d)
+    if d:
+        bt(i+1, -(-r // a[i]) if r < 0 else r // a[i], ad, s, m, d-1)
 
-for op_seq in set(permutations(op, n-1)):
-    result = a[0]
-    for i in range(1, n):
-        if op_seq[i-1] == '+':
-            result += a[i]
-        elif op_seq[i-1] == '-':
-            result -= a[i]
-        elif op_seq[i-1] == '*':
-            result *= a[i]
-        elif op_seq[i-1] == '/':
-            if result < 0:
-                result = -(-result // a[i])
-            else:
-                result //= a[i]
-
-    if result > max_val:
-        max_val = result
-    if result < min_val:
-        min_val = result
-
-print(max_val)
-print(min_val)
+bt(1, a[0], ad, s, m, d)
+print(mx)
+print(mn)
