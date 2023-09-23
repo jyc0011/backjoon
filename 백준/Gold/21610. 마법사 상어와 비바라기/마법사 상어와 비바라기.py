ee1c7b70b492f7d2c6f1e72f6f63e31500216128
jyc0011 <122250128+jyc0011@users.noreply.github.com>
2023-09-23 17:04:28 +0900

    [Gold V] Title: 마법사 상어와 비바라기, Time: 476 ms, Memory: 117168 KB -BaekjoonHub

--- 백준/Gold/21610. 마법사 상어와 비바라기/마법사 상어와 비바라기.py
+++ 백준/Gold/21610. 마법사 상어와 비바라기/마법사 상어와 비바라기.py
@@ -1,35 +1,54 @@
 import sys
+d8 =[[0,0],[0, -1],[-1,-1],[-1, 0],[-1,1],[0, 1],[1,1],[1, 0],[1,-1]]
+d4=[[-1,-1],[-1,1],[1,1],[1,-1]]
 
-d8 = [[0,0], [0, -1], [-1,-1], [-1, 0], [-1,1],
-      [0, 1], [1,1], [1, 0], [1,-1]]
-d4 = [[-1,-1], [-1,1], [1,1], [1,-1]]
+def move(dx, dy, s):
+    global cloud, N, A
+    now_cloud = []
+    for y, x in cloud:
+        ny = (y + dy * s) % N
+        nx = (x + dx * s) % N
+        A[ny][nx] += 1
+        now_cloud.append((ny, nx))
+    water(now_cloud)
+    rain(now_cloud)
 
-def move(dx, dy, s, cloud, N, A):
-    now_cloud = [( (y + dy * s) % N, (x + dx * s) % N) for y, x in cloud]
+
+def water(now_cloud):
+    global A, d4
     for y, x in now_cloud:
-        A[y][x] += 1
-    water(now_cloud, A, N)
-    return rain(now_cloud, A, N)
+        cnt = 0
+        for d in range(4):
+            ny = y + d4[d][0]
+            nx = x + d4[d][1]
+            if ny < 0 or nx < 0 or ny >= N or nx >= N:
+                continue
+            elif A[ny][nx] > 0:
+                cnt += 1
+        A[y][x]+=cnt
 
-def water(now_cloud, A, N):
-    for y, x in now_cloud:
-        cnt = sum(1 for dy, dx in d4
-                  if 0 <= y + dy < N and 0 <= x + dx < N and A[y+dy][x+dx] > 0)
-        A[y][x] += cnt
-
-def rain(now_cloud, A, N):
-    now_cloud_set = set(now_cloud)
-    new_cloud = [(y, x) for y in range(N) for x in range(N) if (y, x) not in now_cloud_set and A[y][x] >= 2]
-    for y, x in new_cloud:
-        A[y][x] -= 2
-    return new_cloud
+def rain(now_cloud):
+    new_cloud = []
+    global cloud, N, A
+    for y in range(N):
+        for x in range(N):
+            if (y,x) in now_cloud or A[y][x] < 2:
+                continue
+            A[y][x]-=2
+            new_cloud.append((y, x))
+    cloud = new_cloud
 
 N ,M = map(int, sys.stdin.readline().rstrip().split())
 A = [list(map(int, sys.stdin.readline().rstrip().split())) for _ in range(N)]
-cloud = [(N-2,0), (N-2,1), (N-1,0), (N-1,1)]
+cloud = [[N-2,0],[N-2,1],[N-1,0],[N-1,1]]
 
 for _ in range(M):
     d,s=map(int, sys.stdin.readline().rstrip().split())
-    cloud = move(d8[d][1], d8[d][0], s, cloud, N, A)
+    move(d8[d][1], d8[d][0], s)
 
-print(sum(sum(row) for row in A))
+# 물의 양 합 계산 후 출력
+result = 0
+for y in range(N):
+    for x in range(N):
+        result += A[y][x]
+print(result)
