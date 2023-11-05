60e43b219041e350abddcaa71502931b2a41e901
jyc0011 <122250128+jyc0011@users.noreply.github.com>
2023-11-05 23:48:49 +0900

    [Gold IV] Title: 연구소, Time: 1860 ms, Memory: 34228 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.py
+++ 백준/Gold/14502. 연구소/연구소.py
@@ -1,46 +1,55 @@
-import copy
+import sys
 from collections import deque
 from itertools import combinations
-n,m = map(int,input().split())
-arr = [list(map(int,input().split())) for _ in range(n)]
-ch = []
-lst_2 = []
 
-dq = deque()
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
 
-def count(lst):
-    cnt = 0
-    for i in range(len(lst)):
-        for j in range(len(lst[i])):
-            if lst[i][j]==0:
-                cnt +=1
-    return cnt
+def spread_virus(walls):
+    q = deque(virus)
+    # 바이러스가 퍼진 위치를 추적
+    spread = set()
+    while q:
+        vx, vy = q.popleft()
+        for dir in range(4):
+            nx, ny = vx + dx[dir], vy + dy[dir]
+            if 0 <= nx < n and 0 <= ny < m and lab[nx][ny] == 0:
+                lab[nx][ny] = 2
+                q.append((nx, ny))
+                spread.add((nx, ny))  # 바이러스가 퍼진 위치 기록
 
-dx = [-1,1,0,0]
-dy = [0,0,-1,1]
+    safe_count = len(empty) - len(spread) - 3  # 안전한 영역 계산 (-3은 벽을 세운 3칸 제외)
+
+    # 퍼진 바이러스와 세웠던 벽을 원래대로 복구
+    for vx, vy in spread:
+        lab[vx][vy] = 0
+    for wx, wy in walls:
+        lab[wx][wy] = 0
+
+    return safe_count
+
+
+# 입력 받기
+n, m = map(int, sys.stdin.readline().rstrip().split())
+lab = [list(map(int, sys.stdin.readline().rstrip().split())) for _ in range(n)]
+
+empty, virus = [], []
+
+# 벽과 바이러스 위치 탐색
 for i in range(n):
     for j in range(m):
-        if arr[i][j] == 0 :
-            ch.append((i,j))
-        if arr[i][j] == 2:
-            lst_2.append((i,j))
+        if lab[i][j] == 0:
+            empty.append((i, j))
+        elif lab[i][j] == 2:
+            virus.append((i, j))
 
 ans = 0
-for i in combinations(ch,3):
-    board = copy.deepcopy(arr)
-    for x,y in i:
-        board[x][y] = 1
-    for a,b in lst_2:
-        dq.append((a,b))
-        while dq:
-            now = dq.popleft()
-            for k in range(4):
-                xx = now[0]+dx[k]
-                yy = now[1]+dy[k]
-                if 0<=xx<n and 0<=yy<m and board[xx][yy] == 0:
-                    board[xx][yy] = 2
-                    dq.append((xx,yy))
-        
-    zero = count(board)
-    ans = max(ans,zero)
+for walls in combinations(empty, 3):
+    # 벽 설치
+    for wx, wy in walls:
+        lab[wx][wy] = 1
+
+    # 바이러스 확산 후 안전 지역 계산
+    ans = max(ans, spread_virus(walls))
+
 print(ans)
