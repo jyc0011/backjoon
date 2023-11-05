02805be356f575ec3e3cbbada72d462add95dbcf
jyc0011 <122250128+jyc0011@users.noreply.github.com>
2023-11-05 22:51:52 +0900

    [Gold IV] Title: Puyo Puyo, Time: 64 ms, Memory: 34132 KB -BaekjoonHub

--- 백준/Gold/11559. Puyo Puyo/Puyo Puyo.py
+++ 백준/Gold/11559. Puyo Puyo/Puyo Puyo.py
@@ -1,55 +1,56 @@
-import sys
 from collections import deque
+import sys
 
-dx = [0, 0, 1, -1]
-dy = [1, -1, 0, 0]
-
-def bfs(x, y, visited, field):
-    queue = deque([(x, y)])
-    visited[x][y] = True
-    color = field[x][y]
-    connected = [(x, y)]
+def bfs(x, y, f):
+    q = deque([(x, y)])
+    c = f[x][y]
+    f[x][y] = '*'
+    count = 1
     
-    while queue:
-        x, y = queue.popleft()
-        for i in range(4):
-            nx, ny = x + dx[i], y + dy[i]
-            if 0 <= nx < 12 and 0 <= ny < 6 and not visited[nx][ny] and field[nx][ny] == color:
-                queue.append((nx, ny))
-                visited[nx][ny] = True
-                connected.append((nx, ny))
-    if len(connected) >= 4:
-        for x, y in connected:
-            field[x][y] = '.'
-        return True
-    return False
-
-def gravity(field):
-    for y in range(6):
-        stack = []
+    while q:
+        x, y = q.popleft()
+        for dx, dy in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+            nx, ny = x + dx, y + dy
+            if 0 <= nx < 12 and 0 <= ny < 6 and f[nx][ny] == c:
+                q.append((nx, ny))
+                f[nx][ny] = '*'
+                count += 1
+    
+    if count >= 4:
         for x in range(12):
-            if field[x][y] != '.':
-                stack.append(field[x][y])
-        for x in range(11, -1, -1):
-            if stack:
-                field[x][y] = stack.pop()
-            else:
-                field[x][y] = '.'
+            for y in range(6):
+                if f[x][y] == '*':
+                    f[x][y] = '.'
+        return True
+    else:
+        for x in range(12):
+            for y in range(6):
+                if f[x][y] == '*':
+                    f[x][y] = c
+        return False
 
-field=[list(sys.stdin.readline().rstrip()) for _ in range(12)]
-cnt=0
+def gravity(f):
+    for y in range(6):
+        bottom = 11
+        for x in range(11, -1, -1):
+            if f[x][y] != '.':
+                if x != bottom:
+                    f[bottom][y], f[x][y] = f[x][y], '.'
+                bottom -= 1
+
+f=[list(sys.stdin.readline().rstrip()) for _ in range(12)]
+cnt = 0
 
 while True:
-    visited = [[False] * 6 for _ in range(12)]
-    chain = False
+    ch = False
     for x in range(12):
         for y in range(6):
-            if field[x][y] != '.' and not visited[x][y]:
-                if bfs(x, y, visited, field):
-                    chain = True
-    if chain:
+            if f[x][y] not in ('.', '*'):
+                if bfs(x, y, f):
+                    ch = True
+    if ch:
         cnt += 1
-        gravity(field)
+        gravity(f)
     else:
         break
 
