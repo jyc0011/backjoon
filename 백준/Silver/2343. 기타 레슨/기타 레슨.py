016f742a1d66316d2b9d94400adb6b6225bb4f33
jyc0011 <122250128+jyc0011@users.noreply.github.com>
2024-02-03 13:22:34 +0900

    [Silver I] Title: 기타 레슨, Time: 216 ms, Memory: 42204 KB -BaekjoonHub

--- 백준/Silver/2343. 기타 레슨/기타 레슨.py
+++ 백준/Silver/2343. 기타 레슨/기타 레슨.py
@@ -1,10 +1,13 @@
 import sys
 
 def required_blu_rays(lectures, max_length):
-    count = 1
+    # 각 블루레이의 최대 길이 'max_length'로 주어질 때
+    # 필요한 블루레이의 수를 계산
+    count = 1  # 최소 한 개의 블루레이는 항상 필요
     current_length = 0
 
     for lecture in lectures:
+        # 현재 블루레이에 더 이상 강의를 추가할 수 없으면 새로운 블루레이 필요
         if current_length + lecture > max_length:
             count += 1
             current_length = 0
@@ -21,6 +24,8 @@ min_length, max_length = max(lectures), sum(lectures)
 
 while min_length <= max_length:
     potential_length = (min_length + max_length) // 2
+    
+    # 'potential_length'로 주어진 길이로 필요한 블루레이 수를 확인
     if required_blu_rays(lectures, potential_length) > m:
         min_length = potential_length + 1
     else:
