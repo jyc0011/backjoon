bc0058ba684896f87891648cbb010697e7047dc7
jyc0011 <122250128+jyc0011@users.noreply.github.com>
2024-04-08 23:18:37 +0900

    [Gold V] Title: 짱해커 이동식, Time: 148 ms, Memory: 124768 KB -BaekjoonHub

--- 백준/Gold/25603. 짱해커 이동식/짱해커 이동식.py
+++ 백준/Gold/25603. 짱해커 이동식/짱해커 이동식.py
@@ -1,13 +1,20 @@
-import sys
 import heapq
+N, K = map(int, input().split())
+cost = list(map(int, input().split()))
 
-input=sys.stdin.readline
+# K==1 -> 가장 비용 높은 거로 하면 됨
+if K == 1:
+    print(max(cost))
+#
+else:
+    ans_li = [] # 구간별 최솟값 저장
+    min_idx = 0 # 슬라이딩 윈도우 시작 인덱스
 
-n,k=map(int, input().rstrip().split())
-cost=list(map(int, input().rstrip().split()))
-heap=[]
+    while min_idx + K <= N: # 슬라이딩 윈도우의 마지막 인덱스가 끝에 도달하지 않음
+        slide = cost[min_idx:min_idx + K] # 현재 슬라이딩 윈도우
+        min_val = min(slide) # 최소 비용 선택
+        min_val_idx = slide.index(min_val) + min_idx # 최소값의 인덱스 찾기
+        min_idx = min_val_idx + 1 # 그거 다음부터 확인 (그 앞은 다 min_val이 최소 비용이니까)
+        heapq.heappush(ans_li, -min_val)
 
-for i in range(n-k+1):
-    li=cost[i:i+k:1]
-    heapq.heappush(heap,-min(li))
-print(-heapq.heappop(heap))
+    print(-heapq.heappop(ans_li))
