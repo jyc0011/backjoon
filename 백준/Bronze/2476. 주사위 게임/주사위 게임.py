27da5940a551195e9d1397be77039b9417b28d33
jyc0011 <122250128+jyc0011@users.noreply.github.com>
2024-04-18 02:02:30 +0900

    [Bronze III] Title: 주사위 게임, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2476. 주사위 게임/주사위 게임.py
@@ -0,0 +1,19 @@
+import sys
+
+ans=0
+
+for _ in range(int(input())):
+    a,b,c=map(int,sys.stdin.readline().rstrip().split())
+
+    if a == b == c:
+        prize = 10000 + a * 1000
+    elif a == b or a == c:
+        prize = 1000 + a * 100
+    elif b == c:
+        prize = 1000 + b * 100
+    else:
+        prize = max(a, b, c) * 100
+
+    ans = max(ans, prize)
+
+print(ans)
