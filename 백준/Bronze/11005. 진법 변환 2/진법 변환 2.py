44bffee8db1da7db7241872819659b5013067f1d
jyc0011 <122250128+jyc0011@users.noreply.github.com>
2025-01-03 21:07:10 +0900

    [Bronze I] Title: 진법 변환 2, Time: 40 ms, Memory: 32544 KB -BaekjoonHub

--- 백준/Bronze/11005. 진법 변환 2/진법 변환 2.py
+++ 백준/Bronze/11005. 진법 변환 2/진법 변환 2.py
@@ -1,10 +1,11 @@
 def convert(n, b):
     T = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    q, r = divmod(n, b)
-    if q == 0:
-        return T[r]
-    else:
-        return convert(q, b) + T[r]
+    ans = []
+    
+    while n > 0:
+        n, r = divmod(n, b)
+        ans.append(T[r])
+    return ''.join(reversed(ans))
 
 N, B = map(int, input().split())
 print(convert(N, B))
