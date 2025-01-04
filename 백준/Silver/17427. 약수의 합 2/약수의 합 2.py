e12f033275d5dceaa7788c4e48609e90d4046507
jyc0011 <122250128+jyc0011@users.noreply.github.com>
2025-01-04 18:19:15 +0900

    [Silver II] Title: 약수의 합 2, Time: 168 ms, Memory: 118392 KB -BaekjoonHub

--- 백준/Silver/17427. 약수의 합 2/약수의 합 2.py
+++ 백준/Silver/17427. 약수의 합 2/약수의 합 2.py
@@ -1,8 +1,12 @@
 def calc_g(n):
+    f=[0]*(n+1)
+    for i in range(1,N+1): # O(n)
+        for j in range(i, N+1, i): # O(n/i) -> O(nlog(n))
+            f[j]+=i
     g=0
     for i in range(1,N+1): # O(n)
-        g+=i*(N//i)
-    return g
+        g+=f[i]
+    return g    
 
 N=int(input())
 print(calc_g(N))
