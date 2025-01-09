22afb965ac0aaad413b2489c82a7f78bb119f04a
jyc0011 <122250128+jyc0011@users.noreply.github.com>
2025-01-09 14:25:17 +0900

    [Silver III] Title: 이번에는 C번이 문자열, Time: 56 ms, Memory: 33432 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/29198. 이번에는 C번이 문자열/이번에는 C번이 문자열.py
+++ 백준/Silver/29198. 이번에는 C번이 문자열/이번에는 C번이 문자열.py
@@ -1,9 +1,33 @@
 import sys
 
 input = sys.stdin.readline
+
+def calc(e):
+    f, _ = e
+    return tuple(-x for x in f)
+
 N, M, K = map(int, input().split())
 S = [input().strip() for _ in range(N)]
-S.sort()
-selected = ''.join(S[:K])
-result = ''.join(sorted(selected))
-print(result)
+arr = []
+sum_ = [0]*26
+ans = []
+
+for s in S:
+    temp = [0]*26
+    for ch in s:
+        temp[ord(ch) - ord('A')] += 1
+    arr.append(temp)
+
+idx = [(arr[i], i) for i in range(N)]
+idx.sort(key=calc)
+chosen = [idx for (_, idx) in idx[:K]]
+
+for i in chosen:
+    for j in range(26):
+        sum_[j] += arr[i][j]
+
+for j in range(26):
+    if sum_[j] > 0:
+        ans.append(chr(ord('A') + j) * sum_[j])
+
+print("".join(ans))
