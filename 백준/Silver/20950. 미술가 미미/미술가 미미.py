4aec0fc2734e50873c0765677b8b2d4187be7aaf
jyc0011 <122250128+jyc0011@users.noreply.github.com>
2025-01-11 19:35:25 +0900

    [Silver II] Title: 미술가 미미, Time: 1888 ms, Memory: 112048 KB -BaekjoonHub

--- 백준/Silver/20950. 미술가 미미/미술가 미미.py
+++ 백준/Silver/20950. 미술가 미미/미술가 미미.py
@@ -1,26 +1,24 @@
 import sys
+from itertools import combinations
 
 input = sys.stdin.readline
 
-def calc(mix_color, target):
-    return abs(mix_color[0] - target[0]) + abs(mix_color[1] - target[1]) + abs(mix_color[2] - target[2])
+def calc(a, b):
+    return abs(a[0] - b[0]) + abs(a[1] - b[1]) + abs(a[2] - b[2])
 
-def mix(idx, depth, r_, g_, b_, cnt):
-    if depth >= 2:
-        mix_color = (r_ // depth, g_ // depth, b_ // depth)
-        diff_val = calc(mix_color, target)
-        cnt = min(cnt, diff_val)
-        if cnt == 0:
-            return cnt
-    if depth == 7 or idx >= n:
-        return cnt
-    r, g, b = paints[idx]
-    cnt = mix(idx + 1, depth + 1, r_ + r, g_ + g, b_ + b, cnt)
-    cnt = mix(idx + 1, depth, r_, g_, b_, cnt)
+def mix(paints, target):
+    cnt = float('inf')
+    for r in range(2, 8):
+        for combo in combinations(paints, r):
+            r_ = sum(color[0] for color in combo) // r
+            g_ = sum(color[1] for color in combo) // r
+            b_ = sum(color[2] for color in combo) // r
+            diff = calc((r_, g_, b_), target)
+            cnt = min(cnt, diff)
     return cnt
 
 n = int(input())
 paints = [tuple(map(int, input().split())) for _ in range(n)]
 target = tuple(map(int, input().split()))
-cnt = mix(0, 0, 0, 0, 0, float('inf'))
+cnt = mix(paints, target)
 print(cnt)
