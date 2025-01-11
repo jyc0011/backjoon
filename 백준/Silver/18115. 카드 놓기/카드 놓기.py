bd66bf1541e3d0e1d2df4db44f89c7607e03ad86
jyc0011 <122250128+jyc0011@users.noreply.github.com>
2025-01-11 22:14:29 +0900

    [Silver III] Title: 카드 놓기, Time: 856 ms, Memory: 96348 KB -BaekjoonHub

--- 백준/Silver/18115. 카드 놓기/카드 놓기.py
+++ 백준/Silver/18115. 카드 놓기/카드 놓기.py
@@ -2,18 +2,16 @@ from collections import deque
 
 n = int(input())
 A = list(map(int, input().split()))
-table = deque(range(1, n+1)) 
 cards = deque()
 
 for i in range(n):
-    top = table.popleft()
     way = A[n-1-i]
     if way == 1:
-        cards.appendleft(top)
+        cards.appendleft(i+1)
     elif way == 2:
         first = cards.popleft()
-        cards.appendleft(top)
+        cards.appendleft(i+1)
         cards.appendleft(first)
     else:
-        cards.append(top)
+        cards.append(i+1)
 print(*cards)
