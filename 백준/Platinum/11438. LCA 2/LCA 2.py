87bbca13a214386fe31b6e80cfcdbcfac681f2ac
jyc0011 <122250128+jyc0011@users.noreply.github.com>
2025-02-27 11:23:29 +0900

    [Platinum V] Title: LCA 2, Time: 684 ms, Memory: 341624 KB -BaekjoonHub

---
+++ 백준/Platinum/11438. LCA 2/LCA 2.py
@@ -0,0 +1,52 @@
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(int(1e5))
+LOG = 21
+
+n = int(input())
+parent = [[0] * LOG for _ in range(n + 1)]
+d = [-1] * (n + 1)
+graph = [[] for _ in range(n + 1)]
+
+for _ in range(n - 1):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+def dfs(x, depth):
+    d[x] = depth
+    for y in graph[x]:
+        if d[y] == -1:
+            parent[y][0] = x
+            dfs(y, depth + 1)
+
+def set_parent():
+    dfs(1, 0)
+    for i in range(1, LOG):
+        for j in range(1, n + 1):
+            if parent[j][i - 1]:
+                parent[j][i] = parent[parent[j][i - 1]][i - 1]
+
+def lca(a, b):
+    if d[a] > d[b]:
+        a, b = b, a
+    diff = d[b] - d[a]
+    for i in range(LOG):
+        if diff & (1 << i):
+            b = parent[b][i]
+    if a == b:
+        return a
+    for i in range(LOG - 1, -1, -1):
+        if parent[a][i] != parent[b][i]:
+            a = parent[a][i]
+            b = parent[b][i]
+
+    return parent[a][0]
+
+set_parent()
+
+m = int(input())
+
+for i in range(m):
+    a, b = map(int, input().split())
+    print(lca(a, b))
