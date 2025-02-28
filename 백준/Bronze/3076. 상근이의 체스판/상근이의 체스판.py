2e36c23e2bc693c8a378ba0a96dfaf0413983e18
jyc0011 <122250128+jyc0011@users.noreply.github.com>
2025-02-28 14:43:09 +0900

    [Bronze II] Title: 상근이의 체스판, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/3076. 상근이의 체스판/상근이의 체스판.py
+++ 백준/Bronze/3076. 상근이의 체스판/상근이의 체스판.py
@@ -1,30 +1,17 @@
 # 완전탐색
+# 문자열로 최적화
 
 import sys
 input = sys.stdin.readline
+write = sys.stdout.write
 
-R,C=map(int,input().split())
-A,B=map(int,input().split())
+R, C = map(int, input().split())
+A, B = map(int, input().split())
 
-# 가장 왼쪽은 검정색이다.
-# R개 | C개 -> A*B
-li=[]
-opposite=[]
-for i in range(C):
-    if i%2==0:
-        for _ in range(B):
-            li.append("X")
-            opposite.append(".")
-    else:
-        for _ in range(B):
-            li.append(".")
-            opposite.append("X")
-            
+row1 = "".join("X" * B if i % 2 == 0 else "." * B for i in range(C))
+row2 = "".join("." * B if i % 2 == 0 else "X" * B for i in range(C))
 
 for i in range(R):
-    if i%2==0:
-        for _ in range(A):
-            print("".join(li))
-    else:
-        for _ in range(A):
-            print("".join(opposite))
+    row_pattern = row1 if i % 2 == 0 else row2
+    for _ in range(A):  
+        write(row_pattern + "\n")
