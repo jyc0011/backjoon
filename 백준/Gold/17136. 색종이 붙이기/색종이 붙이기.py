60782d3faa21e9618021669c7050d0a06684fa9f
jyc0011 <122250128+jyc0011@users.noreply.github.com>
2025-03-04 17:36:36 +0900

    [Gold II] Title: 색종이 붙이기, Time: 384 ms, Memory: 116424 KB -BaekjoonHub

--- 백준/Gold/17136. 색종이 붙이기/색종이 붙이기.py
+++ 백준/Gold/17136. 색종이 붙이기/색종이 붙이기.py
@@ -1,53 +1,44 @@
 import sys
 input = sys.stdin.readline
 
-def dfs(now, used):
+board = [list(map(int, input().split())) for _ in range(10)]
+papers = [0, 5, 5, 5, 5, 5]
+
+ans = float('inf')
+
+def can_attach(x, y, size):
+    if x + size > 10 or y + size > 10:
+        return False
+    for i in range(x, x+size):
+        for j in range(y, y+size):
+            if board[i][j] == 0:
+                return False
+    return True
+
+def attach(x, y, size, value):
+    for i in range(x, x+size):
+        for j in range(y, y+size):
+            board[i][j] = value
+
+def dfs(idx, used):
     global ans
     if used >= ans:
         return
-    if now == 0: 
+    if idx == 100:
         ans = min(ans, used)
         return
-    for pos in range(100):
-        if (now & (1 << pos)) != 0:
-            x, y = divmod(pos, 10)
-            break
-    for size in range(5, 0, -1):
-        if papers[size] == 0:
-            continue
-        masking = paper_mask[x][y][size]
-        if masking == 0:
-            continue
-        if (now & masking) == masking:
-            papers[size] -= 1
-            next = now ^ masking
-            dfs(next, used + 1)
-            papers[size] += 1
+    x = idx // 10
+    y = idx % 10
+    if board[x][y] == 0:
+        dfs(idx+1, used)
+    else:
+        for size in range(5, 0, -1):
+            if papers[size] > 0 and can_attach(x, y, size):
 
-field = 0
-ans = float('inf')
-papers = [0, 5, 5, 5, 5, 5]
-paper_mask = [[[0]*6 for _ in range(10)] for __ in range(10)]
-
-for x in range(10):
-    row = list(map(int, input().split()))
-    for y in range(10):
-        if row[y] == 1:
-            pos = x * 10 + y
-            field |= (1 << pos)
-            
-for x in range(10):
-    for y in range(10):
-        pos = x*10 + y
-        for size in range(1, 6):
-            if x + size > 10 or y + size > 10:
-                paper_mask[x][y][size] = 0
-                continue
-            masking = 0
-            for i in range(x, x+size):
-                for j in range(y, y+size):
-                    masking |= (1 << (i*10 + j))
-            paper_mask[x][y][size] = masking
-
-dfs(field, 0)
+                attach(x, y, size, 0)
+                papers[size] -= 1
+                dfs(idx+1, used+1)
+                attach(x, y, size, 1)
+                papers[size] += 1
+dfs(0, 0)
 print(ans if ans != float('inf') else -1)
