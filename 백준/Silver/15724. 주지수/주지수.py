569828a048a22519bcc4a38632783944627024ed
jyc0011 <122250128+jyc0011@users.noreply.github.com>
2025-03-30 21:18:25 +0900

    [Silver I] Title: 주지수, Time: 900 ms, Memory: 82160 KB -BaekjoonHub

---
+++ 백준/Silver/15724. 주지수/주지수.py
@@ -0,0 +1,15 @@
+import sys
+
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+grid = [list(map(int, input().split())) for _ in range(N)]
+prefix = [[0] * (M + 1) for _ in range(N + 1)]
+for i in range(1, N + 1):
+    for j in range(1, M + 1):
+        prefix[i][j] = (grid[i - 1][j - 1] + prefix[i - 1][j]  + prefix[i][j - 1] - prefix[i - 1][j - 1])
+
+K = int(input())
+for _ in range(K):
+    x1, y1, x2, y2 = map(int, input().split())
+    print(prefix[x2][y2] - prefix[x1 - 1][y2] - prefix[x2][y1 - 1] + prefix[x1 - 1][y1 - 1])
