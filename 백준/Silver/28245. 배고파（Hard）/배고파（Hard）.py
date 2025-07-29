503ca91bf0b052ce4c5521b3333d05891ad56b24
jyc0011 <122250128+jyc0011@users.noreply.github.com>
2025-07-29 11:37:26 +0900

    [Silver III] Title: 배고파(Hard), Time: 156 ms, Memory: 112180 KB -BaekjoonHub

---
+++ 백준/Silver/28245. 배고파（Hard）/배고파（Hard）.py
@@ -0,0 +1,30 @@
+import sys, bisect
+input = sys.stdin.readline
+MAX_ = 61
+pow2 = [1 << i for i in range(MAX_ + 1)]
+vals, pair = [], {}
+for y in range(MAX_ + 1):
+    for x in range(y + 1):
+        temp = pow2[x] + pow2[y]
+        if temp not in pair:
+            pair[temp] = (x, y)
+            vals.append(temp)
+vals.sort()
+
+n = int(input())
+for _ in range(n):
+    m = int(input())
+    idx = bisect.bisect_left(vals, m)
+    num = vals[0]
+    best_diff = abs(num - m)
+    if idx < len(vals):
+        diff = abs(vals[idx] - m)
+        if diff < best_diff or (diff == best_diff and vals[idx] < num):
+            num, best_diff = vals[idx], diff
+    if idx:
+        left = vals[idx - 1]
+        diff = abs(left - m)
+        if diff < best_diff or (diff == best_diff and left < num):
+            num = left
+    x, y = pair[num]
+    print(x,y)
