d86a45b5dbfe055be122f6ca55fcbce7dd286415
jyc0011 <122250128+jyc0011@users.noreply.github.com>
2025-08-02 16:58:03 +0900

    [level 2] Title: 서버 증설 횟수, Time: 0.01 ms, Memory: 9.24 MB -BaekjoonHub

---
+++ 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.py
@@ -0,0 +1,19 @@
+from collections import deque
+
+def solution(players, m, k):
+    answer = 0
+    serverQ=deque([])
+    for _ in range(k):
+        serverQ.append(0)
+    nowS=1
+    
+    for i in range(24):
+        temp=players[i]//m + 1
+        nowS-=serverQ.popleft()
+        if temp - nowS > 0 :
+            answer+=(temp-nowS)
+            serverQ.append(temp-nowS)
+            nowS=temp
+        else :
+            serverQ.append(0)
+    return answer
