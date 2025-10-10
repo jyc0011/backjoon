34ed0728b4379c64d8d06838938a711a8e500b68
jyc0011 <122250128+jyc0011@users.noreply.github.com>
2025-10-10 15:23:45 +0900

    [level 2] Title: 택배 배달과 수거하기, Time: 342.08 ms, Memory: 10.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.py
@@ -0,0 +1,12 @@
+def solution(cap, n, deliveries, pickups):
+    answer = 0
+    d = 0
+    p = 0
+    for i in range(n - 1, -1, -1):
+        d += deliveries[i]
+        p += pickups[i]
+        while d > 0 or p > 0:
+            answer += (i + 1) * 2
+            d -= cap
+            p -= cap
+    return answer
