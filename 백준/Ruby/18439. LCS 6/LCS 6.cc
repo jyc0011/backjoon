ccb5e6f28bbf0d1f5ce89ead3ac0f6567459c05a
jyc0011 <122250128+jyc0011@users.noreply.github.com>
2025-11-02 22:20:46 +0900

    [Ruby V] Title: LCS 6, Time: 192 ms, Memory: 2464 KB -BaekjoonHub

--- 백준/Ruby/18439. LCS 6/LCS 6.cc
+++ 백준/Ruby/18439. LCS 6/LCS 6.cc
@@ -1,87 +1,155 @@
-#include <bits/stdc++.h>
-using namespace std;
+#include <iostream>
+#include <string>
+#include <vector>
+#include <array>
+#include <algorithm>
+#include <numeric>
+#include <cstdint> 
+#include <stdexcept>
 
-using u64 = unsigned long long;
+using std::cin;
+using std::cout;
+using std::string;
+using std::vector;
+using std::array;
+using std::swap;
+
+using u64 = uint64_t;
+
+inline int popcount64(u64 x) {
+    int count = 0;
+    while (x != 0) {
+        x &= (x - 1);
+        count++;
+    }
+    return count;
+}
+
+struct BigBitset {
+    vector<u64> blocks;
+    u64 lastMask;
+    int numWords;
+    BigBitset() : lastMask(0), numWords(0) {}
+    BigBitset(int bits, u64 mask) 
+        : numWords((bits + 63) / 64), lastMask(mask) {
+        blocks.assign(numWords, 0ULL);
+    }
+    
+    BigBitset(vector<u64>&& b, u64 mask) 
+        : blocks(std::move(b)), lastMask(mask), numWords(blocks.size()) {}
+    void maskLast() {
+        if (numWords > 0) {
+            blocks.back() &= lastMask;
+        }
+    }
+    BigBitset operator|(const BigBitset& other) const {
+        vector<u64> res(numWords);
+        for (int i = 0; i < numWords; ++i) {
+            res[i] = blocks[i] | other.blocks[i];
+        }
+        return BigBitset(std::move(res), lastMask);
+    }
+    BigBitset operator&(const BigBitset& other) const {
+        vector<u64> res(numWords);
+        for (int i = 0; i < numWords; ++i) {
+            res[i] = blocks[i] & other.blocks[i];
+        }
+        return BigBitset(std::move(res), lastMask);
+    }
+    BigBitset operator~() const {
+        vector<u64> res(numWords);
+        for (int i = 0; i < numWords; ++i) {
+            res[i] = ~blocks[i];
+        }
+        BigBitset result(std::move(res), lastMask);
+        result.maskLast();
+        return result;
+    }
+
+    BigBitset operator<<(int shift) const {
+        if (shift != 1) {
+            throw std::runtime_error("Only shift-by-1 is implemented");
+        }
+        vector<u64> res(numWords, 0ULL);
+        u64 carry = 0ULL;
+        for (int i = 0; i < numWords; ++i) {
+            u64 nextCarry = blocks[i] >> 63;
+            res[i] = (blocks[i] << 1) | carry;
+            carry = nextCarry;
+        }
+        BigBitset result(std::move(res), lastMask);
+        result.maskLast();
+        return result;
+    }
+
+    BigBitset operator-(const BigBitset& other) const {
+        vector<u64> res(numWords);
+        bool borrow = false;
+        for (int i = 0; i < numWords; ++i) {
+            u64 x = blocks[i];
+            u64 y = other.blocks[i];
+            u64 diff_no_borrow = x - y;
+            res[i] = diff_no_borrow - borrow;
+            bool next_borrow = (x < y) || (borrow && (x == y));
+            borrow = next_borrow;
+        }
+        return BigBitset(std::move(res), lastMask);
+    }
+    
+    void setBit(int i) {
+        int wordIndex = i / 64;
+        int bitIndex = i % 64;
+        if (wordIndex < numWords) {
+            blocks[wordIndex] |= (1ULL << bitIndex);
+        }
+    }
+    
+    int popcount() const {
+        int count = 0;
+        for (u64 block : blocks) {
+            count += popcount64(block);
+        }
+        return count;
+    }
+};
 
 int main() {
-    ios::sync_with_stdio(false);
+    std::ios::sync_with_stdio(false);
     cin.tie(nullptr);
-
     string A, B;
     if (!(cin >> A)) return 0;
     if (!(cin >> B)) return 0;
     string S = A, T = B;
-    if (T.size() > S.size()) swap(S, T);
+    if (T.size() > S.size()) {
+        swap(S, T);
+    }
     const int n = (int)S.size();
     const int m = (int)T.size();
-    const int W = (m + 63) >> 6;
-    const int r = m & 63;
-    const u64 LAST_MASK = (r == 0) ? ~0ULL : ((1ULL << r) - 1ULL);
-
-    auto mask_last = [&](vector<u64>& v) {
-        if (W) v.back() &= LAST_MASK;
-    };
-
-    array<vector<u64>, 26> pos;
-    for (auto &vec : pos) vec.assign(W, 0ULL);
+    if (m == 0) {
+        cout << "0\n";
+        return 0;
+    }
+    const int numWords = (m + 63) / 64;
+    const int lastBlockBits = m % 64;
+    const u64 LAST_MASK = (lastBlockBits == 0) ? ~0ULL : ((1ULL << lastBlockBits) - 1ULL);
+    array<BigBitset, 26> charPosMasks;
+    for (int i = 0; i < 26; ++i) {
+        charPosMasks[i] = BigBitset(m, LAST_MASK);
+    }
     for (int j = 0; j < m; ++j) {
         int c = T[j] - 'A';
-        pos[c][j >> 6] |= (1ULL << (j & 63));
+        charPosMasks[c].blocks[j / 64] |= (1ULL << (j % 64));
     }
-    vector<u64> D(W, 0ULL);
-    auto orV = [&](const vector<u64>& a, const vector<u64>& b) {
-        vector<u64> res(W);
-        for (int i = 0; i < W; ++i) res[i] = a[i] | b[i];
-        return res;
-    };
-    auto andV = [&](const vector<u64>& a, const vector<u64>& b) {
-        vector<u64> res(W);
-        for (int i = 0; i < W; ++i) res[i] = a[i] & b[i];
-        return res;
-    };
-    auto notV = [&](const vector<u64>& a) {
-        vector<u64> res(W);
-        for (int i = 0; i < W; ++i) res[i] = ~a[i];
-        return res;
-    };
-    auto shl1 = [&](const vector<u64>& a) {
-        vector<u64> res(W);
-        u64 carry = 0ULL;
-        for (int i = 0; i < W; ++i) {
-            u64 n = a[i] >> 63;
-            res[i] = (a[i] << 1) | carry;
-            carry = n;
-        }
-        mask_last(res);
-        return res;
-    };
-    auto sub_vec = [&](const vector<u64>& x, const vector<u64>& y) {
-        vector<u64> res(W);
-        unsigned __int128 borrow = 0;
-        for (int i = 0; i < W; ++i) {
-            unsigned __int128 xi = x[i];
-            unsigned __int128 yi = y[i];
-            unsigned __int128 diff = xi - yi - borrow;
-            res[i] = (u64)diff;
-            borrow = (xi < yi + borrow) ? 1 : 0;
-        }
-        return res;
-    };
-
+    BigBitset dpMask(m, LAST_MASK);
     for (int i = 0; i < n; ++i) {
         int c = S[i] - 'A';
-        vector<u64> x = orV(pos[c], D);
-        vector<u64> y = shl1(D);
-        if (W) y[0] |= 1ULL;
-        mask_last(y);
-        vector<u64> tmp = sub_vec(x, y);
-        vector<u64> ntmp = notV(tmp);
-        vector<u64> newD = andV(x, ntmp);
-        mask_last(newD);
-        D.swap(newD);
+        BigBitset x = charPosMasks[c] | dpMask;
+        BigBitset y = dpMask << 1;
+        y.setBit(0);
+        BigBitset tmp_diff = x - y;
+        BigBitset newDpMask = x & (~tmp_diff);
+        dpMask = newDpMask;
     }
-    long long ans = 0;
-    for (int i = 0; i < W; ++i) ans += __builtin_popcountll(D[i]);
-    cout << ans << '\n';
+    cout << dpMask.popcount() << '\n';
     return 0;
 }
